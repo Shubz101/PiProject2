a130d54f08391c76084707fee90884e068eecb85
Shubz101 <shubzzz1019@gmail.com>
2024-10-25 00:35:59 +0530

    Update PaymentMethods.tsx

--- app/page2/PaymentMethods.tsx
+++ app/page2/PaymentMethods.tsx
@@ -28,7 +28,7 @@ export default function PaymentMethods() {
   {
     id: 'kucoin',
     name: 'KuCoin',
-    image: '/public/kucoin.jpg', // Update the image path
+    image: 'https://imgur.com/gDaDsRH', // Update the image path
     displayText: 'KuCoin',
     isConnected: false,
     placeholder: 'Enter KuCoin address'
